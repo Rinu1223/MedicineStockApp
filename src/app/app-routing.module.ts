5bcbd627a9661df51878f86837bcddf387ccc3aa
krishna96-tecdune <krishna.malik@tecdune.com>
2021-07-09 17:31:49 +0530

    Components Added & Login Form UI Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { MedicineDetailsComponentComponent } from './medicine-details-component/medicine-details-component.component';
+import { SignupComponentComponent } from './signup-component/signup-component.component';
+
+const routes: Routes = [
+	{ path: '', component: LoginComponentComponent  },
+	{ path: 'signup', component: SignupComponentComponent },
+	{ path: 'medicine-details', component: MedicineDetailsComponentComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [
+		RouterModule.forRoot(routes)
+	],
+	exports: [
+		RouterModule
+	]
 })
 export class AppRoutingModule { }
---
+++ src/app/medicine-details-component/medicine-details-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-medicine-details-component',
+  templateUrl: './medicine-details-component.component.html',
+  styleUrls: ['./medicine-details-component.component.css']
+})
+export class MedicineDetailsComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
